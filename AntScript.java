53e30683e723c9f449d73993b3a8814faff80971
Kailash Pathak <kpathak@bellarmine.edu>
2019-09-14 16:40:53 -0400

    added files

---
+++ AntScript.java
@@ -0,0 +1,35 @@
+
+/**
+*Reading auth file and printing summary output
+*@ author Kailash Pathak
+* @version 1.0
+* Programming Project One
+* CS322 - Compiler Construction
+* Spring 2019
+*/
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class AntScript {
+
+	public static void main(String[] args) throws FileNotFoundException{
+//creating file instance to refer  text file
+try{
+System.out.println("Enter the file name  you want to process :");
+Scanner scnr= new Scanner(System.in);
+File file = new File (scnr.nextLine());
+scnr = new Scanner(file);
+while (scnr.hasNextLine()){
+String line = scnr.nextLine();
+System.out.println(line);
+}
+scnr.close();
+}
+catch (Exception ex){
+ex.printStackTrace();
+}
+}
+}
+
