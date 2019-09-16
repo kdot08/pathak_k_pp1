4a3d2a9e2135bd80dfc32c4e55bcf2a16dd156b3
Kailash Pathak <kpathak@bellarmine.edu>
2019-09-16 00:56:24 -0400

    Succesfully implemented hashmap and if and switch cases; updated xml

--- AntScript.java
+++ AntScript.java
@@ -14,23 +14,54 @@ import java.io.FileNotFoundException;
 
 public class AntScript {
        static  Regex regex = new Regex();
-	public static void main(String[] args) throws FileNotFoundException{
-//creating file instance to refer  text file
+	static int counter = 0;
+	public static void main(String[] args){readFile(args[0]);
+String userFlag = args[1];
+		int flag = Integer.parseInt(userFlag);
+		switch(flag) {
+		case 0:
+			System.out.println(counter + " lines in the log file were parsed.");
+			System.out.println("There are " + regex.mapSize(regex.ip) + " unique IP addresses in the log.");
+			System.out.println("There are " + regex.mapSize(regex.users) + " unique users in the log.");
+			break;
+		case 1:
+			regex.traverseMap(regex.ip);
+			System.out.println(counter + " lines in the log file were parsed.");
+			System.out.println("There are " + regex.mapSize(regex.ip) + " unique IP addresses in the log.");
+			System.out.println("There are " + regex.mapSize(regex.users) + " unique users in the log.");
+			break;
+		case 2:
+			regex.traverseMap(regex.users);
+			System.out.println(counter + " lines in the log file were parsed.");
+			System.out.println("There are " + regex.mapSize(regex.ip) + " unique IP addresses in the log.");
+			System.out.println("There are " + regex.mapSize(regex.users) + " unique users in the log.");
+			break;
+		default:
+			System.out.println(counter + " lines in the log file were parsed.");
+			System.out.println("There are " + regex.mapSize(regex.ip) + " unique IP addresses in the log.");
+			System.out.println("There are " + regex.mapSize(regex.users) + " unique users in the log.");
+		}
+	System.out.println("finished switch");
+}
+static void readFile(String filename){
+
 try{
-System.out.println("Enter the file name  you want to process :");
-Scanner scnr= new Scanner(System.in);
-File file = new File (scnr.nextLine());
-scnr = new Scanner(file);
-while (scnr.hasNextLine()){
-String line = scnr.nextLine();
-regex.isValidIP(line);
-regex.isValidUser(line);
-}
-scnr.close();
-}
-catch (Exception ex){
-ex.printStackTrace();
-}
-}
+	System.out.println("Enter the file name  you want to process :");
+	Scanner scnr= new Scanner(System.in);
+		File file = new File (scnr.nextLine());
+	scnr = new Scanner(file);
+	while (scnr.hasNextLine()){
+		String line = scnr.nextLine();
+		regex.isValidIP(line);
+	regex.isValidUser(line);
+	counter++;
+		}
+		scnr.close();
+		
+		}
+catch (FileNotFoundException e) {
+        e.printStackTrace();
+    }
+ }
 }
 
--- Regex.java
+++ Regex.java
@@ -9,29 +9,48 @@
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
- 
+import java.util.HashMap;
+import java.util.Map; 
 public class Regex {
+	static HashMap<String, Integer> ip = new HashMap<String, Integer>();
+	static HashMap<String, Integer> users = new HashMap<String, Integer>();
 static  void  isValidIP(String ipAddr){
-         
+  
         Pattern ptn = Pattern.compile("(((\\d{3})|(\\d{2}))[.]){3}((\\d{3})|(\\d{2}))");
-      
+  
  Matcher mtch = ptn.matcher(ipAddr);
-        if( mtch.find()){
-
-
-        System.out.println(mtch.group(0));
-}
+        if(mtch.find()) {
+				if(ip.containsKey(mtch.group(0))) {
+					int count = ip.get(mtch.group(0));
+					ip.put(mtch.group(0), count + 1);
+				} else {
+					ip.put(mtch.group(0), 1);
+				}
+			}
 }
 static void isValidUser(String ipUser){
 Pattern ptn1 = Pattern.compile("user (([a-z]+|[A-Z]+|[0-9]+)+)");
 Matcher mtch1 = ptn1.matcher(ipUser);
- if( mtch1.find()){
-
-
-        System.out.println(mtch1.group(0));
-
+ if(mtch1.find()) {
+			String buffer = mtch1.group(0).substring(5);
+			
+				if(users.containsKey(buffer)) {
+					int count = users.get(buffer);
+					users.put(buffer, count + 1);
+				} else {
+					users.put(buffer, 1);
+				}
+			}
 }
-}
+static int mapSize(HashMap<String, Integer> map) {
+		return map.size();
+	}
+
+	static void traverseMap(HashMap<String, Integer> map) {
+		for (String i : map.keySet() ) {
+			System.out.println(i + " :" + map.get(i)); 
+		}
+	}
 }
 
 
